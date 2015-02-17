f1a0622dc8ffb89b89d8b237fe4778f5e26b3a30
Joel Kaplan <joelk511@gmail.com>
2015-02-17 14:02:50 -0500

    Installed bootstrap and styled nav area

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
-#The purpose of a helper method is to use ruby code to produce html in a view.
+# The purpose of a helper method is to use ruby code to produce html in a view. 
+# In this case, tell anytime that <%= my_name %> is used to add Joel Kaplan.
 
 	def my_name
 		"<h3>Joel Kaplan</h3>".html_safe
