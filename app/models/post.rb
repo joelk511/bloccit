cd72413e36963e516adb107f22a743d5a7edd77a
Joel Kaplan <joelk511@gmail.com>
2015-03-02 12:40:22 -0500

    Changes to post.rb file

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 
 	default_scope { order('created_at DESC') }
-	scope :ordered_by_title, -> { unscoped.order("title ASC") }
-	scope :ordered_by_reverse_created_at, -> { unscoped.order('created_at DESC').reverse_order }
+	# scope :ordered_by_title, -> { unscoped.order("title ASC") }
+	# scope :ordered_by_reverse_created_at, -> { unscoped.order('created_at DESC').reverse_order }
 
 end
 
