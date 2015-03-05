e553c74d4922ce4fe99e57714f050c37ee1d1e9d
Joel Kaplan <joelk511@gmail.com>
2015-03-05 12:24:34 -0700

    Completed Authorizations and Added Admin Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ include Pundit
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-    protected
+  rescue_from Pundit::NotAuthorizedError do |exception|
+  	redirect_to root_url, alert: exception.message
+  end
+
+  protected
 
   def configure_permitted_parameters
   	devise_parameter_sanitizer.for(:sign_up) << :name
