e10ff46c854e8c7b1102704ecb1ba83d89aa5c73
Joel Kaplan <joelk511@gmail.com>
2015-03-10 15:30:31 -0400

    Associate and nest topics and posts.

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,7 +1,25 @@
 class PostPolicy < ApplicationPolicy
 
-	def index?
-		true 
-	end
+  def index?
+	true 
+  end
+
+  class Scope
+  	attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def policy_scope
+    	if user.admin? || user.moderator?
+    		scope.all
+    	else
+    	scope.where(:id => user.id).exists?
+    	end
+    end
+  end
+
 
 end
