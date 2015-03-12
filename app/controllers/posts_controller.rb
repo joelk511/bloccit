d460c165a1845aa8520f4511b1800ccf1e8e4e0d
Joel Kaplan <joelk511@gmail.com>
2015-03-12 16:35:50 -0400

    Complete post markdown assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,8 +43,24 @@ class PostsController < ApplicationController
       end
     end
 
+  def render_as_markdown(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
+  
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def markdown_title
+    render_as_markdown
+  end
+
+  def markdown_body
+    render_as_markdown
   end
 
 end
