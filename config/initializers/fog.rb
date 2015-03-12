03ebbdbac927ab9f652c54edd08ef335c8296058
Joel Kaplan <joelk511@gmail.com>
2015-03-12 17:55:30 -0400

    Upload image changes part 3

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,9 @@
-CarrierWave.configure do |cofig|
-	config.fog_credentials = {
-		provider:             'AWS',
-		aws_access_key_id:     ENV['AKIAJLJMPJ53UOVKQCBQ'],
-		aws_secret_access_key: ENV['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']
-	}
-	config.fog_directory = ENV['joelkaplanbloccitdevelopment']
-	config.fog_public    = true
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:               'AWS',
+    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY']
+  }
+  config.fog_directory  = ENV['AWS_BUCKET']
+  config.fog_public     = true
 end
