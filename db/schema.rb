8c7175e86476ecc7bacd37253b6811b45d2a032b
Joel Kaplan <joelk511@gmail.com>
2015-03-16 14:01:41 -0400

    Uploading-Images Assignment Progress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312205248) do
+ActiveRecord::Schema.define(version: 20150316170148) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -34,9 +34,11 @@ ActiveRecord::Schema.define(version: 20150312205248) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -45,18 +47,17 @@ ActiveRecord::Schema.define(version: 20150312205248) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.integer  "summary_id"
   end
 
+  add_index "posts", ["summary_id"], name: "index_posts_on_summary_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "summaries", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "post_id"
   end
-
-  add_index "summaries", ["post_id"], name: "index_summaries_on_post_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
