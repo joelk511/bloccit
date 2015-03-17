caec0aa9817f253ccd6b84b652ca51d6cd85c42e
Joel Kaplan <joelk511@gmail.com>
2015-03-17 12:26:42 -0400

    Deleted additional migrations that were no longer needed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313191353) do
+ActiveRecord::Schema.define(version: 20150317162144) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -47,10 +47,26 @@ ActiveRecord::Schema.define(version: 20150313191353) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.integer  "summary_id"
   end
 
+  add_index "posts", ["summary_id"], name: "index_posts_on_summary_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "subcomments", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "comment_id"
+  end
+
+  add_index "subcomments", ["comment_id"], name: "index_subcomments_on_comment_id"
+
+  create_table "summaries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -79,6 +95,7 @@ ActiveRecord::Schema.define(version: 20150313191353) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
