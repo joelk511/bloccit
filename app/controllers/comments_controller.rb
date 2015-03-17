2d5c9d7e65fb9f449469861ef3e1e56943ada040
Joel Kaplan <joelk511@gmail.com>
2015-03-16 22:37:13 -0400

    Finished Comments Checkpoint

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,15 +7,20 @@ class CommentsController < ApplicationController
 
 def create
   @post = Post.find(params[:post_id])
-  @comment = @post.comments.create(params[:id])
+  @comment = @post.comments.create(comment_params)
   @comment.user_id = current_user.id
+
   if @comment.save 
       flash[:notice] = "Comment was saved."
-      render :new
+      redirect_to [@post.topic, @post]
   else
       flash[:error] = "There was an error saving the comment. Please try again."
   end
 end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
 
 
