d2e21ceba3f42b81931d98ff88b13623d14aa135
Joel Kaplan <joelk511@gmail.com>
2015-03-19 21:01:18 -0400

    Rspec Assignment Complete

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Vote do 
+	describe "validations" do
+		
+		describe "value validation" do
+			it "only allows -1 or 1 as values" do
+				expect( @vote.value_validation).to eq(1 || -1)
+			end
+		end
+	end
+end
+
+		
