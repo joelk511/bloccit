c079ab0d78c9f57f5c459cd2211559974345424a
Joel Kaplan <joelk511@gmail.com>
2015-03-22 13:34:12 -0400

    Pagination Assignment Complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
     def create
       @topic = Topic.find(params[:topic_id])
       @post = current_user.posts.build(post_params)
-      @topic = post.topic
+      @post.topic = @topic
       authorize @post
       if @post.save 
         flash[:notice] = "Post was saved."
