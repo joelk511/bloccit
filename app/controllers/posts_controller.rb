79c8a27f3bcc3ddf50e7640cede344cd5a1f66c4
Joel Kaplan <joelk511@gmail.com>
2015-03-22 11:58:41 -0400

    Fixed Post Error: topic blank

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,12 @@ class PostsController < ApplicationController
   end
 
   def create
+    
+
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(post_params)
     authorize @post
+    @post.topic = @topic
       if @post.save 
         flash[:notice] = "Post was saved."
         redirect_to [@topic, @post]
