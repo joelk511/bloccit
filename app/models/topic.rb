a4d5a57013da1f6e1b6e281d663d2f65b666731d
Joel Kaplan <joelk511@gmail.com>
2015-04-05 23:11:44 -0400

    Private Topics Assignment Progress

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,20 @@ class Topic < ActiveRecord::Base
 	has_many :posts, dependent: :destroy 
 
 	scope :visible_to, -> (user) { user ? all : where(public: true) }
+
+	scope :publicly_viawable, -> (user) { user ? all : where(public: true) } #publicly_viawable
+
+	scope :privately_viewable, -> (user) { user ? all : where(public: false) }#privately_viewable
+
+	def publicly_viewable
+	end
+
+	def privately_viewable
+	end
+
+	def visible_to(user)
+	end
+
+
 end
+
