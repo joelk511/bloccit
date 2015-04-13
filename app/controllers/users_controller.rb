e862568cb95f584365dc2122dc4cd6ca5eea13d4
Joel Kaplan <joelk511@gmail.com>
2015-04-13 11:45:23 -0400

    Public Profile Checkpoint Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:show]
+
+	def show
+     @user = User.find(params[:id])
+     @posts = @user.posts
+     @comments = @user.comments
+	end
 
 	def update
 		if current_user.update_attributes(user_params)
@@ -16,4 +22,5 @@ class UsersController < ApplicationController
 	def user_params
 		params.require(:user).permit(:name, :avatar)
 	end
+
 end
